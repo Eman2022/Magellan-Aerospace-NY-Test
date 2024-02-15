431c767f078747000ad4dd80041673ebfaeae1da
Erich Lamontagne <eman2881@outlook.com>
2024-02-14 22:10:16 -0800

    Post/Get functions added 0.3

--- MagellanTest/Controllers/ItemsController.cs
+++ MagellanTest/Controllers/ItemsController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using Npgsql;
 using System.Data;
+using System.Xml.Linq;
 
 namespace MagellanTest.Controllers
 {
@@ -9,41 +11,81 @@ namespace MagellanTest.Controllers
     public class ItemsController : ControllerBase
     {
 
-        string connectionString = "Host=localhost;Port=5432;Password=remoter;Database=Part";
-        NpgsqlConnection connection;
+        string connectionString = "Host=localhost;Port=5432;User Id=postgres;Password=remoter;Database=part";
+        
         
 
-
-        private void Connect()
+        private NpgsqlConnection Get_Connection()
         {
-            connection = new NpgsqlConnection(connectionString);
+            NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
 
-            if(connection.State == ConnectionState.Closed)
+
+        [HttpGet]
+        [Route("getTotalCost")]
+        public String Get_Total_Cost(string item_name)
+        {
+            //TODO: protect against injection
+            DataTable l = GetData($"SELECT Get_Total_Cost('{item_name}') from item WHERE item_name = '{item_name}';");
+            return l.ToString();
+        }
+        
+        
+        [HttpGet]
+        [Route("test")]
+        public String test()
+        {
+            DataTable l = GetData("select * from item;");
+            return l.ToString();
+        }
+
+
+        [HttpPost]
+        [Route("Insert")]
+        public async Task<IActionResult> SetData(string name, int parent_id, int cost, DateOnly dt)
+        {
+            try
             {
-                connection.Open();
+                await using var connection = Get_Connection();
+                await connection.OpenAsync();
+
+                var sql = @"INSERT INTO item (name, parent_id, cost, req_date) VALUES (@Name, @ParentId, @Cost, @Date)";
+
+                await using var cmd = new NpgsqlCommand(sql, connection);
+                cmd.Parameters.AddWithValue("Name", name);
+                cmd.Parameters.AddWithValue("ParentId", parent_id);
+                cmd.Parameters.AddWithValue("Cost", cost);
+                cmd.Parameters.AddWithValue("Date", dt);
+
+                await cmd.ExecuteNonQueryAsync();
+
+                return Ok("Insert success");
             }
-
-            
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Insert fail: {ex.Message}");
+            }
         }
 
-        private void test()
-        {
-            DataTable l = GetData(new string("select * from Item;"));
-            Console.WriteLine(l.ToString());
-        }
 
         protected DataTable GetData(string sql)
         {
-            Connect();
-            DataTable dt = new DataTable();
+            
+            using (NpgsqlConnection connection = Get_Connection())
+            {
+                DataTable dt = new DataTable();
 
-            NpgsqlCommand command = new NpgsqlCommand();
-            command.Connection = connection;
-            command.CommandText = sql;
+                NpgsqlCommand command = new NpgsqlCommand(sql, connection);
 
-            NpgsqlDataReader reader = command.ExecuteReader();
-            dt.Load(reader);
-            return dt;
+                using (NpgsqlDataReader reader = command.ExecuteReader())
+                {
+                    dt.Load(reader);
+                    return dt;
+                }
+            }
+
         }
     }
 }
--- MagellanTest/obj/Debug/net6.0/MagellanTest.AssemblyInfo.cs
+++ MagellanTest/obj/Debug/net6.0/MagellanTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagellanTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a3e7501de539326d64bfecc65adbc9ae690afd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da1fd5f65e7330d1a543a783d44f3d4885a09f54")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagellanTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagellanTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
